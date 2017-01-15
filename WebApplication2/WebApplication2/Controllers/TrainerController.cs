47728f1912fd87ae818778802072e1327d1616e4
Jerzy Szachniewicz <szczur.londowy666@gmail.com>
2017-01-15 23:16:24 +0100

    Excercise list

---
+++ WebApplication2/WebApplication2/Controllers/TrainerController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    public class TrainerController : Controller
+    {
+        // GET: Trainer
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult ExcerciseList()
+        {
+            IEnumerable<Exercise> myList;
+            using (var dbContext = new Model1())
+            {
+                myList = dbContext.Exercise.ToList();
+            }
+            return View(myList);
+        }
+    }
+}
